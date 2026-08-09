9f17e996316c8156f5d68471f7a200ab0aa17d50
Mayank Chaudhary <chaudharymayank0307@gmail.com>
2026-08-09 21:46:05 +0530

    code modified

--- child_module/bastion/main.tf
+++ child_module/bastion/main.tf
@@ -1,4 +1,9 @@
 resource "azurerm_bastion" "bstion" {
 
-   
+   for_each = var.bastion
+   name                = each.value.name
+   resource_group_name = each.value.resource_group_name
+    location            = each.value.location
+
+    
 }
